904478166a4b62160a1b09ec9a9362fd8732d695
hsiao-chi Wu <ciyalnin@gmail.com>
2017-08-13 23:36:25 +0800

    Update index.php
    add link to LINE

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 require_once('LineBotVIV.php');
 
-$channelAccessToken = 'Your Channel Access Token';
-$channelSecret = 'Your Channel Secret';
+$channelAccessToken = 'gHsZCt17+VcM/XoK8Bw2zrd+yKNIDH/RxlpWIsN5vpybMVwIqBKvykooJTBHHqYLTwQIF1JrPNH/QvNAWIZFF2abPucIBPVm2qDzQcH1HFNZB0roxPrQpoa6dPp4qvDqXAwClWQIAM1vWsFbwLLI+wdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '	
+3e004ec129a589b5feaf1c3d44c7315a';
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 
