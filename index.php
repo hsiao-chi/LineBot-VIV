569f94c732c8753517ded8393f15c5ef26618785
hsiao-chi Wu <ciyalnin@gmail.com>
2017-08-13 23:26:11 +0800

    Update index.php
    start to test Line Bot

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-  echo "six gravity VIV"
+require_once('LineBotVIV.php');
+
+$channelAccessToken = 'Your Channel Access Token';
+$channelSecret = 'Your Channel Secret';
+
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+
+foreach ($client->parseEvents() as $event) {
+    $client->replyMessage(array(
+        'replyToken' => $event['replyToken'],
+        'messages' => array(
+            array(
+                'type' => 'text',
+                'text' => $message['text']
+            )
+        )
+    ));
+};
 ?>
