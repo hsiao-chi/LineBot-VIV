8a7bb596b1a34f02d454e29a5510d06d0f7061eb
hsiao-chi Wu <ciyalnin@gmail.com>
2017-08-15 21:17:05 +0800

    Update LineBotVIV.php

--- LineBotVIV.php
+++ LineBotVIV.php
@@ -37,13 +37,13 @@ class LINEBotTiny
 
     public function parseEvents()
     {
-        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+       /* if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
             echo 405;
             http_response_code(405);
             error_log("Method not allowed");
             exit();
             
-        }
+        }*/
 
         $entityBody = file_get_contents('php://input');
 
